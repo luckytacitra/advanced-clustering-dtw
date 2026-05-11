249d10c4b617f263e976cd8a504a208791d4faf9
luckytacitra <6003251012@student.its.ac.id>
2026-05-11 12:47:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,26 @@ st.title("🚗 Advanced Time Series Clustering with DTW")
 # Load dataset
 df = pd.read_csv("dataset.csv")
 
+# Convert datetime
+df['LastUpdated'] = pd.to_datetime(df['LastUpdated'])
+
+# Create Occupancy Rate
+df['OccupancyRate'] = df['Occupancy'] / df['Capacity']
+
 # Show dataset
 st.subheader("Dataset Preview")
 
 st.dataframe(df.head())
+
+st.subheader("Occupancy Rate Preview")
+
+st.dataframe(
+    df[['SystemCodeNumber',
+        'Occupancy',
+        'Capacity',
+        'OccupancyRate']]
+    .head()
+)
 
 # Show basic info
 st.subheader("Dataset Information")
