84cc0ebf79756f8f11750f57159b836570d9c8f6
luckytacitra <6003251012@student.its.ac.id>
2026-05-11 12:43:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🚗 Advanced Clustering with DTW")
+# Title
+st.title("🚗 Advanced Time Series Clustering with DTW")
 
-st.write("Dashboard Time Series Clustering menggunakan DTW")
+# Load dataset
+df = pd.read_csv("dataset.csv")
+
+# Show dataset
+st.subheader("Dataset Preview")
+
+st.dataframe(df.head())
+
+# Show basic info
+st.subheader("Dataset Information")
+
+st.write("Jumlah Data:", df.shape[0])
+st.write("Jumlah Kolom:", df.shape[1])
+
+# Show columns
+st.subheader("Columns")
+
+st.write(df.columns.tolist())
